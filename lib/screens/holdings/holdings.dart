1e0c66f2a9fa0679a688f001d9146cbab5b5067c
Joshua Garcia <josshuagarciia000@gmail.com>
2021-05-15 11:55:52 -0700

    Cleaned holdings screen.

--- lib/screens/holdings/holdings.dart
+++ lib/screens/holdings/holdings.dart
@@ -3,7 +3,7 @@ import 'package:flutter_fadein/flutter_fadein.dart';
 
 import 'package:simulador/models/marketPrices.dart';
 import 'package:simulador/screens/holdings/empty.dart';
-import 'package:simulador/screens/trading/trading.dart';
+
 import 'package:simulador/services/database.dart';
 import 'package:simulador/services/holdings.dart';
 import 'package:simulador/shared/common/portfolioBalance.dart';
@@ -36,95 +36,20 @@ class _HoldingsScreenState extends State<HoldingsScreen>  {
               child: FadeIn(
                 duration: Duration(milliseconds: 300, ),
                 child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  SizedBox(height: 24,),
-                  Text('Posiciones',
-                    style: screenTitle,
-                  ),
-                  SizedBox(height: 24),
-                  
-                  PortfolioBalance(actionEnabled: false),
-                  SizedBox(height: 24,),
-                  this._buildPortfolioSubtitle(title: 'Cartera', ),
-                  SizedBox(height: 4),
-
-                  StreamBuilder(
-                    stream: Database().holdings,
-                    builder: (BuildContext context, AsyncSnapshot<DocumentSnapshot> snapshot) {
-
-                      if (snapshot.hasData) {
-                        final dbtickers = snapshot.data['holdings'];
-                        List<String> list = [];
-                        dbtickers.forEach((hodl) {
-                          list.add(hodl['ticker']);
-                        });
-
-                        print(list);
-
-                        print(snapshot.data['holdings']);
-
-                        if (list.isNotEmpty) {
-                          final tickers = list.join(',');
-
-                          print(tickers);
-
-                          return FutureBuilder(
-                          future: _holdingsService.fetchHoldings(tickers),
-                          builder: (context, snapshot) {
-                            if (snapshot.hasData) {
-                              final itemCount = snapshot.data.length;                              
-                              return ListView.builder(
-                                shrinkWrap: true,
-                                physics: NeverScrollableScrollPhysics(),
-                                itemCount: itemCount,
-                                itemBuilder: (context, idx) {
-                                
-                                  final MarketPricesModel price = snapshot.data[idx]; 
-
-                                  return Padding(
-                                    padding: EdgeInsets.symmetric(vertical: 8),
-                                    child: StockCard( 
-                                      ticker: price.symbol, 
-                                      companyName: price.name, 
-                                      change: price.change,
-                                       price: price.price
-                                      ),
-                                  );
-                                },
-                              );
-                            } else {
-                              return Padding(
-                                padding: EdgeInsets.symmetric(vertical: 100),
-                                child: Center(
-                                  child: CircularProgressIndicator(),
-                                )
-                              );
-                            }
-                          }
-                          );
-                        } else {
-                          return HoldingsEmpty();
-                        }
-
-
-                      } else {
-                        return Padding(
-                          padding: EdgeInsets.symmetric(vertical: 100),
-                          child: Center(
-                            child: CircularProgressIndicator(),
-                          )
-                        );
-                      }
-
-                    }
-                  ),
-
-                  
-
-
-                ],
-              )
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    SizedBox(height: 24,),
+                    Text('Posiciones',
+                      style: screenTitle,
+                    ),
+                    SizedBox(height: 24),
+                    PortfolioBalance(actionEnabled: false),
+                    SizedBox(height: 24,),
+                    this._buildPortfolioSubtitle(title: 'Cartera', ),
+                    SizedBox(height: 4),
+                    this._buildStreambuilder()
+                  ],
+                )
               )
             ),
           )
@@ -165,5 +90,75 @@ class _HoldingsScreenState extends State<HoldingsScreen>  {
     );
   }
 
+  Widget _buildStreambuilder() {
+    return StreamBuilder(
+      stream: Database().holdings,
+      builder: (BuildContext context, AsyncSnapshot<DocumentSnapshot> snapshot) {
+        if (snapshot.hasData) {
+          final dbtickers = snapshot.data['holdings'];
+          List<String> list = [];
+
+          dbtickers.forEach((hodl) =>  list.add(hodl['ticker']));
+
+          print(list);
+          print(snapshot.data['holdings']);
+          if (list.isNotEmpty) {
+            final tickers = list.join(',');
+
+            return _buildFuturebuilder(tickers);
+
+          } else {
+            return HoldingsEmpty();
+          }
+          
+        } else {
+          return Padding(
+            padding: EdgeInsets.symmetric(vertical: 100),
+            child: Center(
+              child: CircularProgressIndicator(),
+            )
+          );
+        }
+      }
+    );
+  }
+
+  Widget _buildFuturebuilder(String tickers) {
+
+    final defaultWidget = Padding(
+      padding: EdgeInsets.symmetric(vertical: 100),
+      child: Center(child: CircularProgressIndicator())
+    );
+
+    return FutureBuilder(
+      future: _holdingsService.fetchHoldings(tickers),
+      builder: (context, snapshot) {
+        if (snapshot.hasData) {
+          final itemCount = snapshot.data.length;                              
+          return ListView.builder(
+            shrinkWrap: true,
+            physics: NeverScrollableScrollPhysics(),
+            itemCount: itemCount,
+            itemBuilder: (context, idx) {
+            
+              final MarketPricesModel price = snapshot.data[idx]; 
+
+              return Padding(
+                padding: EdgeInsets.symmetric(vertical: 8),
+                child: StockCard( 
+                  ticker: price.symbol, 
+                  companyName: price.name, 
+                  change: price.change,
+                  price: price.price
+                ),
+              );
+            },
+          );
+        } else {
+          return defaultWidget;
+        }
+      }
+    );
+  }
  
 }
